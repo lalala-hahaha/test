002f1172168190f7c819b68721cf5d9ed4587b70
chendongge <cccbrook@gmail.com>
2025-01-15 01:08:22 +0800

    feat: 修改历史页面跳客服

--- multi-page-project/src/pages/inital/js/index.js
+++ multi-page-project/src/pages/inital/js/index.js
@@ -1,5 +1,6 @@
+
+
 const pageid = "papaya-marzipan-3d20b6";
-const waNo = "62882001014790";
 async function initializePage() {
   try {
     const response = await fetch(
@@ -9,14 +10,41 @@ async function initializePage() {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
     const data = await response.json();
+    console.log(data[pageid]);
+    (function (w, d, e, x) {
+      w[e] = function () {
+        w.cbk = w.cbk || [];
+        w.cbk.push(arguments);
+      };
+      x = d.createElement("script");
+      x.async = true;
+      x.id = "zhichiScript";
+      x.className = "btn";
+      x.src =
+        "https://sg.sobot.com/chat/frame/v2/entrance.js?sysnum=0609ebe3231142b8a815d7bd1831d3f3";
+      d.body.appendChild(x);
+    })(window, document, "zc");
     if (data && data[pageid] === "1") {
-      window.location.href = `https://wa.me/${waNo}`;
+      zc("config", {
+        cardTrigger: 1,
+        type: 2,
+        channelid: 2,
+        custom: true,
+        man_trace: true,
+        auto_expand: true,
+      });
+    }else{
+      zc("config", {
+        cardTrigger: 1,
+        type: 2,
+        channelid: 2,
+        custom: true,
+        man_trace: true,
+        auto_expand: false,
+      });
     }
   } catch (error) {
     console.error("Fetch error:", error);
   }
 }
 initializePage();
-const affiliation = function () {
-  kwaiq.instance("262327847787656").track("contentView");
-};
