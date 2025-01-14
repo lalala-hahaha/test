09eb695c7d8e96b996a416febdb274d1808d98ac
陈东阁 <chendongge@MacBook-Pro.local>
2025-01-14 23:33:11 +0800

    feat: 智齿客服

---
+++ multi-page-project/src/pages/zckf/js/index.js
@@ -0,0 +1,22 @@
+const pageid = "papaya-marzipan-3d20b6";
+const waNo = "62882001014790";
+async function initializePage() {
+  try {
+    const response = await fetch(
+      "https://mr4xa3bnuh567e5gbmbeyl5xdq0lpdoh.lambda-url.ap-southeast-1.on.aws/"
+    );
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const data = await response.json();
+    if (data && data[pageid] === "1") {
+      window.location.href = `https://wa.me/${waNo}`;
+    }
+  } catch (error) {
+    console.error("Fetch error:", error);
+  }
+}
+initializePage();
+const affiliation = function () {
+  kwaiq.instance("262327847787656").track("contentView");
+};
