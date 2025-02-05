a5f1f557a61329cc0df0c7186c2adf2ed940a0e8
chendongge <chendongge@taqu.cn>
2025-02-05 17:04:39 +0800

    feat: 方案b

--- multi-page-project/src/pages/recruit_b/js/index.js
+++ multi-page-project/src/pages/recruit_b/js/index.js
@@ -1,51 +1,60 @@
-const pageid = "silly-maamoul-d6c673";
-let switchValue = "0";
-async function initializePage() {
-  try {
-    const response = await fetch(
-      "https://mr4xa3bnuh567e5gbmbeyl5xdq0lpdoh.lambda-url.ap-southeast-1.on.aws/"
-    );
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    const data = await response.json();
-    if (data && data[pageid] === "1") {
-      finalPage();
-      switchValue = "1";
-    }
-  } catch (error) {
-    console.error("Fetch error:", error);
-  }
-}
+const pageid = "";
 
 // 为按钮添加点击事件监听器
 document.getElementById("age-not").addEventListener("click", function () {
   document.getElementById("page-age").style.display = "none";
   document.getElementById("page-age-not").style.display = "flex";
+  gtag('event', 'button_click', {
+    'button_name': 'age-not',
+    'button_value': '18-'
+  });
+  fbq('track', 'B_18-');
 });
 document.getElementById("age-yes").addEventListener("click", function () {
   document.getElementById("page-age").style.display = "none";
   document.getElementById("page-sex").style.display = "flex";
+  gtag('event', 'button_click', {
+    'button_name': 'age-yes',
+    'button_value': '18+'
+  });
+  fbq('track', 'B_18+');
 });
 document.getElementById("sex-male").addEventListener("click", function () {
   document.getElementById("page-sex").style.display = "none";
   document.getElementById("page-form").style.display = "flex";
+  gtag('event', 'button_click', {
+    'button_name': 'sex-male',
+    'button_value': 'male'
+  });
+  fbq('track', 'B_male');
 });
 document.getElementById("sex-female").addEventListener("click", function () {
-  if (switchValue == "1") {
-    document.getElementById("page-sex").style.display = "none";
-    document.getElementById("page-welcome").style.display = "flex";
-  } else {
-    document.getElementById("page-sex").style.display = "none";
-    document.getElementById("page-form").style.display = "flex";
-  }
-});
-document
-  .getElementById("form-submit-btn")
-  .addEventListener("click", function () {
-    document.getElementById("page-form").style.display = "none";
-    document.getElementById("page-thank").style.display = "flex";
+  document.getElementById("page-sex").style.display = "none";
+  document.getElementById("page-welcome").style.display = "flex";
+  gtag('event', 'button_click', {
+    'button_name': 'sex-female',
+    'button_value': 'female'
   });
+  fbq('track', 'B_female');
+});
+document.getElementById("form-submit-btn").addEventListener("click", function () {
+  document.getElementById("page-form").style.display = "none";
+  document.getElementById("page-thank").style.display = "flex";
+  gtag('event', 'button_click', {
+    'button_name': 'form-submit-btn',
+    'button_value': 'submit_form'
+  });
+  fbq('track', 'B_submit_form');
+});
+document.getElementById("welcome-link").addEventListener("click", function () {
+  document.getElementById("page-form").style.display = "none";
+  document.getElementById("page-thank").style.display = "flex";
+  gtag('event', 'button_click', {
+    'button_name': 'welcome-link',
+    'button_value': 'welcome'
+  });
+  fbq('track', 'B_welcome');
+});
 
 // 获取当前wa列表
 async function fetchWaLinks() {
@@ -79,4 +88,4 @@ async function finalPage() {
 }
 
 // 调用初始化函数
-initializePage();
+finalPage();
