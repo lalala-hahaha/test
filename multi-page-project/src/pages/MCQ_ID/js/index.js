bd62c1674047cc526c34af44eab0688a45eee228
chendongge <cccbrook@gmail.com>
2025-02-05 21:54:32 +0800

    feat: 去掉年龄选择，修改链接为按钮样式

--- multi-page-project/src/pages/MCQ_ID/js/index.js
+++ multi-page-project/src/pages/MCQ_ID/js/index.js
@@ -19,24 +19,6 @@ async function initializePage() {
 }
 
 // 为按钮添加点击事件监听器
-document.getElementById("age-not").addEventListener("click", function () {
-  document.getElementById("page-age").style.display = "none";
-  document.getElementById("page-age-not").style.display = "flex";
-  gtag('event', 'button_click', {
-    'button_name': 'age-not',
-    'button_value': '18-'
-  });
-  fbq('track', '18-');
-});
-document.getElementById("age-yes").addEventListener("click", function () {
-  document.getElementById("page-age").style.display = "none";
-  document.getElementById("page-sex").style.display = "flex";
-  gtag('event', 'button_click', {
-    'button_name': 'age-yes',
-    'button_value': '18+'
-  });
-  fbq('track', '18+');
-});
 document.getElementById("sex-male").addEventListener("click", function () {
   document.getElementById("page-sex").style.display = "none";
   document.getElementById("page-form").style.display = "flex";
@@ -70,8 +52,6 @@ document.getElementById("form-submit-btn").addEventListener("click", function ()
   fbq('track', 'submit_form');
 });
 document.getElementById("welcome-link").addEventListener("click", function () {
-  document.getElementById("page-form").style.display = "none";
-  document.getElementById("page-thank").style.display = "flex";
   gtag('event', 'button_click', {
     'button_name': 'welcome-link',
     'button_value': 'welcome'
@@ -106,7 +86,7 @@ async function finalPage() {
       console.log(targetUrl);
       const targetEle = document.getElementById("welcome-link");
       targetEle.href = targetUrl;
-      targetEle.innerText = targetUrl;
+      // targetEle.innerText = targetUrl;
     }
   } catch (error) {
     console.error("Failed to initialize toggles:", error);
--- multi-page-project/src/pages/recruit_b/js/index.js
+++ multi-page-project/src/pages/recruit_b/js/index.js
@@ -1,24 +1,6 @@
 const pageid = "charming-melba-31ae66";
 
 // 为按钮添加点击事件监听器
-document.getElementById("age-not").addEventListener("click", function () {
-  document.getElementById("page-age").style.display = "none";
-  document.getElementById("page-age-not").style.display = "flex";
-  gtag('event', 'button_click', {
-    'button_name': 'age-not',
-    'button_value': '18-'
-  });
-  fbq('track', 'B_18-');
-});
-document.getElementById("age-yes").addEventListener("click", function () {
-  document.getElementById("page-age").style.display = "none";
-  document.getElementById("page-sex").style.display = "flex";
-  gtag('event', 'button_click', {
-    'button_name': 'age-yes',
-    'button_value': '18+'
-  });
-  fbq('track', 'B_18+');
-});
 document.getElementById("sex-male").addEventListener("click", function () {
   document.getElementById("page-sex").style.display = "none";
   document.getElementById("page-form").style.display = "flex";
@@ -47,8 +29,6 @@ document.getElementById("form-submit-btn").addEventListener("click", function ()
   fbq('track', 'B_submit_form');
 });
 document.getElementById("welcome-link").addEventListener("click", function () {
-  document.getElementById("page-form").style.display = "none";
-  document.getElementById("page-thank").style.display = "flex";
   gtag('event', 'button_click', {
     'button_name': 'welcome-link',
     'button_value': 'welcome'
@@ -83,7 +63,7 @@ async function finalPage() {
       console.log(targetUrl);
       const targetEle = document.getElementById("welcome-link");
       targetEle.href = targetUrl;
-      targetEle.innerText = targetUrl;
+      // targetEle.innerText = targetUrl;
     }
   } catch (error) {
     console.error("Failed to initialize toggles:", error);
