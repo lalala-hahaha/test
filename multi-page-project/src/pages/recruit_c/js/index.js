b2a61a7e7960ad8a94c9bbfc8e3ed26b59ea5494
chendongge <chendongge@taqu.cn>
2025-02-10 11:29:20 +0800

    feat: 修改事件名

--- multi-page-project/src/pages/recruit_c/js/index.js
+++ multi-page-project/src/pages/recruit_c/js/index.js
@@ -14,7 +14,7 @@ document.getElementById("sex-female").addEventListener("click", function () {
   gtag('event', 'button_female');
   fbq('track', 'C_female');
   ttq.track('C_female');
-  ttq.track('ClickButton');
+  ttq.track('ViewContent')
 });
 document.getElementById("form-submit-btn").addEventListener("click", function () {
   document.getElementById("page-form").style.display = "none";
@@ -30,7 +30,7 @@ document.getElementById("welcome-link").addEventListener("click", function () {
   });
   fbq('track', 'C_welcome');
   ttq.track('C_welcome');
-  ttq.track('Contact');
+  ttq.track('ClickButton');
 });
 
 // 获取当前wa列表
