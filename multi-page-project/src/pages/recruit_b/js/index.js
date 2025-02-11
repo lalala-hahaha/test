0eebf22f11c6eceb9fc6c7c857229d2d6766fa25
chendongge <cccbrook@gmail.com>
2025-02-12 01:20:44 +0800

    feat: 修改charming-melba-31ae66获取链接方式

--- multi-page-project/src/pages/recruit_b/js/index.js
+++ multi-page-project/src/pages/recruit_b/js/index.js
@@ -4,23 +4,19 @@ const pageid = "charming-melba-31ae66";
 document.getElementById("sex-male").addEventListener("click", function () {
   document.getElementById("page-sex").style.display = "none";
   document.getElementById("page-form").style.display = "flex";
-  gtag('event', 'button_male');
   fbq('track', 'B_male');
 });
 document.getElementById("sex-female").addEventListener("click", function () {
   document.getElementById("page-sex").style.display = "none";
   document.getElementById("page-welcome").style.display = "flex";
-  gtag('event', 'button_female');
   fbq('track', 'B_female');
 });
 document.getElementById("form-submit-btn").addEventListener("click", function () {
   document.getElementById("page-form").style.display = "none";
   document.getElementById("page-thank").style.display = "flex";
-  gtag('event', 'button_submit');
   fbq('track', 'B_submit_form');
 });
 document.getElementById("welcome-link").addEventListener("click", function () {
-  gtag('event', 'button_welcome');
   fbq('track', 'Lead', {
     event_source_url: window.location.href,
   });
@@ -36,6 +32,9 @@ async function fetchWaLinks() {
       headers: {
         "Content-Type": "application/json",
       },
+      body: JSON.stringify({
+        action: 'getList',
+      }),
     }
   );
   const data = await response.json();
@@ -46,8 +45,8 @@ async function fetchWaLinks() {
 async function finalPage() {
   try {
     const data = await fetchWaLinks();
-    if (data?.url) {
-      const targetUrl = data.url;
+    if (data?.fbLinks) {
+      const targetUrl = data.fbLinks[0];
       console.log(targetUrl);
       const targetEle = document.getElementById("welcome-link");
       targetEle.href = targetUrl;
