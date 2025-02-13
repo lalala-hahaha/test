ebebaa5609ad0deb976846b3b21b105fd7ce6af4
chendongge <chendongge@taqu.cn>
2025-02-13 12:11:18 +0800

    feat: tk曝光页面

---
+++ multi-page-project/src/pages/recruit_tk_exposure_a/js/index.js
@@ -0,0 +1,54 @@
+const targetPlatform = "TK";
+// function getQueryParam(name) {
+//   const match = window.location.search.match(new RegExp(`[?&]${name}=([^&]*)`));
+//   return match ? decodeURIComponent(match[1]) : null;
+// }
+
+// const customPlatform = getQueryParam('platform');
+
+// if (customPlatform) {
+//   targetPlatform = customPlatform
+// }
+
+// 获取当前wa列表
+async function fetchWaLinks(targetList) {
+  const response = await fetch(
+    `https://wjqicpjvr34cvmzucfiocae3ie0irrxb.lambda-url.ap-southeast-1.on.aws/`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        action: "getList",
+        target: targetList,
+      }),
+    }
+  );
+  const data = await response.json();
+  return data;
+}
+
+// 初始化函数
+async function finalPage() {
+  try {
+    const data = await fetchWaLinks(targetPlatform);
+    if (data?.links) {
+      const targetUrl = data.links[0];
+      console.log(targetUrl);
+      const targetEle = document.getElementById("welcome-link");
+      targetEle.href = targetUrl;
+      // targetEle.innerText = targetUrl;
+    }
+  } catch (error) {
+    console.error("Failed to initialize toggles:", error);
+  }
+}
+
+// 调用初始化函数
+finalPage();
+
+// 为按钮添加点击事件监听器
+document.getElementById("welcome-link").addEventListener("click", function () {
+  ttq.track("D_welcome");
+});
