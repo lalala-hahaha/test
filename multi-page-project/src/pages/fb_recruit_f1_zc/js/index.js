aec515992fbfa95c50ebf814f773f41eb2cf5d74
chendongge <cccbrook@gmail.com>
2025-03-30 16:05:14 +0800

    feat: 修改zc落地页

--- multi-page-project/src/pages/fb_recruit_f1_zc/js/index.js
+++ multi-page-project/src/pages/fb_recruit_f1_zc/js/index.js
@@ -45,7 +45,7 @@ function bindButtonEvents() {
     sexMaleButton.addEventListener("click", () => {
       document.getElementById("page-sex").style.display = "none";
       document.getElementById("page-thank").style.display = "flex";
-      fbq("track", "B_male");
+      fbq("track", "G_male");
     });
   }
   
@@ -53,7 +53,7 @@ function bindButtonEvents() {
     sexFemaleButton.addEventListener("click", () => {
       console.log('目标用户，联系客服')
       fbq("track", "Lead", { event_source_url: window.location.href });
-      fbq("track", "B_welcome");
+      fbq("track", "G_welcome");
     });
   }
 }
@@ -62,17 +62,6 @@ function bindButtonEvents() {
 function initialize() {
   finalLinks();
   bindButtonEvents();
-  // zc("config",{
-  //   custom:true, //设置自定义生效 第二步
-  //   locale:"id", //id-印尼语，en-英文，cn-简体中文，tw-繁体中文（默认简体中文）
-  //   type: 4, // 客服接待模式 1仅机器人客服 2仅人工客服 3机器人客服优先 4人工客服优先
-  //   msg_flag: '0', // 结束会话后是否显示留言入口 1关闭 0开启
-  //   photo_flag: '1', // 上传附件按钮 仅H5 1开启 0关闭
-  //   source: '4', // 来源 0:桌面网站,1:微信,2:APP,3:微博,4:移动网站,9：企业微信,10：微信小程序
-  //   tip_title: '', // 邀请文案
-  //   submit_title: '',// 接受邀请按钮
-  //   expand_size: 'half',// 规定在H5侧，聊天组件弹出高度。half为半屏，full为全屏，默认为全屏
-  // })
 }
 
 // 等待 DOM 加载完成后初始化
