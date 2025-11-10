3434da6d0e4792cf7c1f4e475a6c61ed1e10e265
chendongge <cccbrook@gmail.com>
2025-11-11 01:08:45 +0800

    11

---
+++ multi-page-project/pages/LK_baxi/kw_general/README.md
@@ -0,0 +1,9 @@
+# 页面信息说明
+
+- 创建时间：2025-11-05 22:05:36
+
+- 最后修改时间：2025-11-05 22:10:51
+
+- 链接：https://stellar-cassata-a8b59f.netlify.app/
+
+- 页面ID：stellar-cassata-a8b59f
---
+++ multi-page-project/pages/LK_baxi/kw_general/es6/index.js
@@ -0,0 +1,28 @@
+const pageId = "stellar-cassata-a8b59f";
+let linkIndex;
+try {
+  const val = getQueryParam("idx");
+  // 转成数字，且判断有效数字，否则默认1
+  linkIndex = Number(val);
+  if (isNaN(linkIndex) || linkIndex < 1) {
+    linkIndex = 1;
+  }
+} catch (e) {
+  linkIndex = 1;
+}
+const eventStr = "";
+
+// 页面初始化逻辑
+function initialize() {
+  // 获取 WA 链接列表
+  finalLinks(linkIndex);
+  // 绑定按钮点击事件
+  bindButtonEvents(eventStr);
+  // 剩余名额
+  thePlaces();
+  // 倒计时
+  startFlashSaleCountdown();
+}
+
+// 等待 DOM 加载完成后初始化
+document.addEventListener("DOMContentLoaded", initialize);
