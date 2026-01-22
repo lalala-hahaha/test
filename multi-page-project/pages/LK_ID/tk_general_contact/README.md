3c2de3286d07fe423346385a21bd0b0f8888cc57
chendongge <cccbrook@gmail.com>
2026-01-22 22:39:09 +0800

    tk contact

---
+++ multi-page-project/pages/LK_ID/tk_general_contact/README.md
@@ -0,0 +1,9 @@
+# 页面信息说明
+
+- 创建时间：2025-08-04 16:53:13
+
+- 最后修改时间：2026-01-22 22:38:50
+
+- 链接：https://curious-malasada-3e4483.netlify.app/
+
+- 页面ID：curious-malasada-3e4483
---
+++ multi-page-project/pages/LK_ID/tk_general_contact/js/index.js
@@ -0,0 +1,30 @@
+"use strict";
+
+var pageId = "curious-malasada-3e4483";
+var linkIndex;
+try {
+  var val = getQueryParam("idx");
+  // 转成数字，且判断有效数字，否则默认1
+  linkIndex = Number(val);
+  if (isNaN(linkIndex) || linkIndex < 1) {
+    linkIndex = 1;
+  }
+} catch (e) {
+  linkIndex = 1;
+}
+var eventStr = "";
+
+// 页面初始化逻辑
+function initialize() {
+  // 获取 WA 链接列表
+  finalLinks(linkIndex);
+  // 绑定按钮点击事件
+  bindButtonEvents(eventStr);
+  // 剩余名额
+  thePlaces();
+  // 倒计时
+  startFlashSaleCountdown();
+}
+
+// 等待 DOM 加载完成后初始化
+document.addEventListener("DOMContentLoaded", initialize);
